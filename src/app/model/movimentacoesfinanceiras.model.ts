64486358b7c2f519ce1d7d6f9af1d26fa91ed865
GuiChristo <guilherme.lagunde@ubistart.com>
2023-11-01 19:22:19 -0300

    commit inicial

---
+++ src/app/model/movimentacoesfinanceiras.model.ts
@@ -0,0 +1,7 @@
+export class MovimentacoesFinanceiras{
+    id: number;
+    valor: number;
+    tipo: number;
+    data: Date;
+    descricao: string;
+}
---
+++ src/app/service/movimentacoesfinanceiras.service.ts
@@ -0,0 +1,40 @@
+import { environment } from "src/environments/environment.development";
+import { Service } from "./service";
+import { HttpClient } from "@angular/common/http";
+import { MovimentacoesFinanceiras } from "../model/movimentacoesfinanceiras.model";
+import { Observable, map, retry } from "rxjs";
+import { ApiResponse } from "../model/apiresponse.model";
+
+export class MovimentacoesFinanceirasService extends Service {
+    private baseUrl = `${environment.apiUrl}/movimentacoesfinanceiras`;
+
+    constructor(private http: HttpClient) {
+        super();
+    }
+
+    save(movimentacaoFinanceira: MovimentacoesFinanceiras): Observable<ApiResponse> {
+        return this.http.post<ApiResponse>((this.baseUrl), movimentacaoFinanceira
+        ).pipe(
+            retry(1)//permite fazer quantas tentativas quiser
+        )
+    }
+
+    getList(termoBusca: string, datainicio: string, datafim: string, tipo: string): Observable<ApiResponse> {
+        return this.http.get<ApiResponse>(this.baseUrl + '?pesquisa=' + termoBusca + `&datainicio=${datainicio}&datafim=${datafim}&tipo=${tipo}`)
+            .pipe(retry(10) //Permite que possamos colocar o número de tentativas
+            )
+    }
+
+    getOne(id: string): Observable<ApiResponse> {
+        return this.http.get(`${this.baseUrl}/${id}`)
+            .pipe(retry(10), //Permite que possamos colocar o número de tentativas
+                map((resposta: ApiResponse) => resposta)) // Conseguimos assim transformar o retorno para um objeto - Somente o conteudo da resposta
+        //Utilizamos o MAP para transformar no objeto esperado.
+    }
+
+    delete(id : string) : Observable<ApiResponse>{
+        return this.http.delete( `${this.baseUrl}/${id}`)
+        .pipe(map((resposta:ApiResponse) => resposta)) // Conseguimos assim transformar o retorno para um objeto - Somente o conteudo da resposta
+              //Utilizamos o MAP para transformar no objeto esperado.
+    }
+}    
